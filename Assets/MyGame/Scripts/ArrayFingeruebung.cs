f623c2d5a592d173ee08754f70e394c41fbcb696
nwendlinger <114598279+nwendlinger@users.noreply.github.com>
2022-11-14 18:08:10 +0100

    Add files via upload
    SecondUpload

--- Assets/MyGame/Scripts/ArrayFingeruebung.cs
+++ Assets/MyGame/Scripts/ArrayFingeruebung.cs
@@ -5,4 +5,27 @@ using UnityEngine;
 public class ArrayFingeruebung : MonoBehaviour
 {
     int[] array;
+
+    int[] array1 = new int[4];
+    int[] array2 = new int[] { 21, 22, 23, 24 };
+    int[] array3 = { 31, 32, 33, 34, 35, 36, 37 };
+    string[] array4 = new[]
+    {
+        "array4",
+        "array4",
+        "array4",
+        "array4",
+        "array4",
+        "array4",
+        "array4",
+        "array4",
+        "array4",
+        "array4",
+    };
+
+    private void Start()
+    {
+        GameObject[] redImageArray = GameObject.FindGameObjectsWithTag("RedImages");
+    }
+    
 }
